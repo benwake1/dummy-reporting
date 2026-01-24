4e1d505cae22d73583e569cea127e20478579a79
benwake1 <108348418+benwake1@users.noreply.github.com>
2026-01-24 22:21:41 +0000

    added a force fail test

--- cypress/e2e/demo.cy.js
+++ cypress/e2e/demo.cy.js
@@ -3,3 +3,20 @@ describe('Dummy test', () => {
     expect(true).to.equal(true)
   })
 })
+
+describe('Demo Failing Test', () => {
+before(() => {
+// optional: visit any page
+cy.visit('https://example.cypress.io')
+})
+
+
+it('Intentionally fails to demo screenshots and videos', () => {
+// This assertion will always fail
+cy.get('h1').should('contain', 'THIS TEXT DOES NOT EXIST')
+
+
+// Another failing assertion
+cy.get('body').should('have.class', 'non-existent-class')
+})
+})
