681551afa0dfe9898d48deef6efeaf2f01a929a6
Ben Wake <benwake1@outlook.com>
2026-01-25 20:28:56 +0000

    Adding Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+export default {
+    content: [
+        './.cypress-dashbord/**/*.html',
+        './gh-pages/**/*.html',
+        './mochawesome-report/**/*.html',
+    ],
+    theme: {
+        extend: {},
+    },
+};
