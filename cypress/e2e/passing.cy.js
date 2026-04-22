0ad78536e069960e341540fb9a01471d82b85792
benwake1 <108348418+benwake1@users.noreply.github.com>
2026-04-22 21:32:16 +0100

    Add passing and failing Cypress test demo

---
+++ cypress/e2e/passing.cy.js
@@ -0,0 +1,18 @@
+describe('Passing Test Demo', () => {
+  it('always passes', () => {
+    expect(true).to.equal(true)
+  })
+})
+
+describe('Demo Passing Test', () => {
+before(() => {
+// optional: visit any page
+cy.visit('https://example.cypress.io')
+})
+
+
+it('Intentionally fails to demo screenshots and videos', () => {
+// This assertion will always fail
+cy.get('h1').should('contain', 'Kitchen Sink')
+
+})
